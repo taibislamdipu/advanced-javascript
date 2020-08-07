71ca8e3e2fe8bd9c4f7962383e5f6df13c1ae7ec
taibislamdipu <mailtaibislam@gmail.com>
2020-08-07 18:49:36 +0600

    few uses of map & filter

--- object-property.js
+++ object-property.js
@@ -7,16 +7,17 @@ const student = [
     { id: 71, name: "DeepJol" }
 ];
 
-// const names = student.map(x => x.name);
-// const ids = student.map(s => s.id);
+const names = student.map(x => x.name);
+const ids = student.map(s => s.id > 40);
 const bigger = student.filter(s => s.id > 40);
-// const biggerOne = student.find(s => s.id > 40);
+const biggerOne = student.find(s => s.id > 40);
 
-const idOver40 = student.map(s => s.id > 40);
+const bigger2 = student.filter(s => ({ 'id': s.id > 40 })); // same jinis ektu diffrent vabe kora
+const idOver40 = student.filter(s => s.id > 40).map(s => s.id) // find the specific value from array
 
-// console.log(names);
-// console.log(ids);
-// console.log(bigger);
-// console.log(biggerOne);
-console.log(idOver40);
+console.log(names);
+console.log(ids);
 console.log(bigger);
+console.log(biggerOne);
+console.log(bigger2);
+console.log(idOver40);
