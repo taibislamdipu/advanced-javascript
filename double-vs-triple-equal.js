0b8aeca3136a9b4777b184ccc3ce46bf8d694029
taibislamdipu <mailtaibislam@gmail.com>
2020-08-07 15:41:01 +0600

    double-vs-triple-equal

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,9 +1,19 @@
 
-
+// == only check only check condition
 const firstNum = 1;
 const secondNum = true;
 
 if (firstNum == secondNum) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+// === check condition & data type
+const firstNum = 2;
+const secondNum = "2";
+
+if (firstNum === secondNum) {
     console.log("condition is true");
 } else {
     console.log("condition is false");
